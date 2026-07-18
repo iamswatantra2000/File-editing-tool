b65d484b6f666138630a1dec0d7f51a8d98dece0
Swatantra Gupta <iamswatantra2000@gmail.com>
2026-07-18 13:15:21 +0530

    Adding the deployed backend URL on vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:8000",
+      "/api": "file-editing-tool-production.up.railway.app",
     },
   },
 });
